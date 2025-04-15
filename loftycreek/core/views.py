cce1e1d48ec49e00d6d139ca5843d4fc5b9b035b
Brian Orandi <brianorandi97@gmail.com>
2025-04-15 16:32:02 +0300

    Added about, academics, admissions, contact pages

--- loftycreek/core/views.py
+++ loftycreek/core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+# index view
+def index(request):
+    return render(request, 'core/index.html')
+
+# about view
+def about(request):
+    return render(request, 'core/about.html')
+
+# academics view
+def academics(request):
+    return render(request, 'core/academics.html')
+
+# admissions view
+def admissions(request):
+    return render(request, 'core/admissions.html')
+
+# contact us view
+def contact(request):
+    return render(request, 'core/contact.html')
